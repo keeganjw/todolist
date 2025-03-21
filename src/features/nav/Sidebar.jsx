a8a3f5838345cfb0f8461e247069e7e8fd3a7e1c
Keegan Woodward <keeganjw@users.noreply.github.com>
2025-03-20 21:16:28 -0400

    Added Tailwind

--- src/features/nav/Sidebar.jsx
+++ src/features/nav/Sidebar.jsx
@@ -1,16 +1,30 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function Sidebar() {
-	return (
-		<nav>
-			<ul>
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="/todos">Todo Lists</Link>
-				</li>
-			</ul>
-		</nav>
-	);
+  return (
+    <aside className="h-screen w-20">
+      <nav className="flex h-full flex-col border-r bg-white shadow-sm">
+        <h3 className="flex items-center justify-between p-4">Tenor</h3>
+
+        <ul></ul>
+      </nav>
+    </aside>
+  );
 }
+
+// import { Link } from 'react-router-dom';
+
+// export default function Sidebar() {
+// 	return (
+// 		<nav>
+// 			<ul>
+// 				<li>
+// 					<Link to="/">Home</Link>
+// 				</li>
+// 				<li>
+// 					<Link to="/todos">Todo Lists</Link>
+// 				</li>
+// 			</ul>
+// 		</nav>
+// 	);
+// }
--- src/features/todoList/TodoList.jsx
+++ src/features/todoList/TodoList.jsx
@@ -1,21 +1,21 @@
-import TodoItem from './TodoItem';
+import TodoItem from "./TodoItem";
 
 export default function TodoList({ todos, toggleTodo, deleteTodo }) {
-	return (
-		<>
-			<ul className="list">
-				{todos.length === 0 && 'None'}
-				{todos.map((todo) => {
-					return (
-						<TodoItem
-							{...todo}
-							toggleTodo={toggleTodo}
-							deleteTodo={deleteTodo}
-							key={todo.id}
-						/>
-					);
-				})}
-			</ul>
-		</>
-	);
+  return (
+    <>
+      <ul className="list">
+        {todos.length === 0 && "None"}
+        {todos.map((todo) => {
+          return (
+            <TodoItem
+              {...todo}
+              toggleTodo={toggleTodo}
+              deleteTodo={deleteTodo}
+              key={todo.id}
+            />
+          );
+        })}
+      </ul>
+    </>
+  );
 }
